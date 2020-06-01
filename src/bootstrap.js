d93b822cd3cd15db0512a7f8218527b9b77718ce
Zackery Bayle <Zackery.bayle@gmail.com>
2020-06-01 15:39:37 -0700

    setup results commpnent

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/home";
+import Results from "./components/results";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -16,7 +17,7 @@ function main() {
       <BrowserRouter>
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/results' component={Home} />
+          <Route path='/results' component={Results} />
 
         </Switch>
       </BrowserRouter>
---
+++ src/components/results.js
@@ -0,0 +1,16 @@
+import React, { Componet } from 'react';
+import Logo from '../components/logo';
+
+
+class Results extends Componet {
+    render() {
+        return(
+            <div>
+                <h1>Results</h1>
+                <Logo />
+            </div>
+        )
+    }
+}
+
+export default Results;
